da01de3cd32f6bb21c11644b5049be5d3ab97297
Marc Solà Nadal <msola@edpuzzle.com>
2024-08-24 11:49:40 +0200

    fix: removed junk files

--- default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
-  overrides = poetry2nix.overrides.withDefaults (self: super: {
-    foo = foo.overridePythonAttrs (oldAttrs: {});
-  });
-}
